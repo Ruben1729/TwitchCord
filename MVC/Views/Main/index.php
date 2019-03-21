2aac0b5c8b68f9bc76af523c1f08134f2a899863
Michael Cannucci <gameactin@gmail.com>
2019-03-21 14:01:02 -0400

    Intergrate Vue into the workflow

--- MVC/Views/Main/index.php
+++ MVC/Views/Main/index.php
@@ -10,11 +10,18 @@
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 		<title>TwitchCord</title>
-
+		<link href=Vue/chunk-vendors.js rel=preload as=script>
+		<link href=Vue/css/index.css rel=preload as=style>
+		<link href=Vue/index.js rel=preload as=script>
+		<link href=Vue/css/index.css rel=stylesheet>
 	</head>
 	<body>
-		<form>
-
-		</form>
+	<noscript>
+	We're sorry, this website needs javascript to function
+	</noscript>
+	<div id=app></div>
+	<script src=Vue/chunk-vendors.js></script>
+	<script src=Vue/index.js></script>
+	<link>
 	</body>
 </html>
