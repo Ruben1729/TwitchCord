ef406d732472deab01205d846cc87981a83c93d2
cstuser <cstuser>
2019-03-26 12:15:50 -0400

    Login and Register css

--- MVC/Views/User/login.php
+++ MVC/Views/User/login.php
@@ -1,18 +1,67 @@
-<?php
-	require "welcome_page.header.php";
+<?php 
+
+	
 ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Header for the main page after the user has logged in.">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<link rel="stylesheet" href="/CSS/UserAccessForm.css">
+
+		<title>TwitchCord</title>
+	</head>
 	<body>
 		<main>
-			<form action="Login" method="post">
-				<input type="text" name="uid" placeholder="Username">
-				<input type="password" name="pwd" placeholder="Password">
-				<div>Forgot your password?</div>
+			<form id="login-form" action="Login" method="post">
+				<h1>Welcome Back!</h1>
+				<div id="userinput">
+					<div id="input-container">
+						<?php
+
+							if(empty($error)){
+								echo "<label for=\"uid\">Username</label>";
+								echo "<input type=\"text\" name=\"uid\">";
+							}
+							else{
+								echo "<div id=\"error\"><label for=\"uid\">Username</label> - ".$error[0]."</div>"; 
+								echo "<input id=\"inerror\" type=\"text\" name=\"uid\">";
+
+							}
+
+						?>
+					</div>
+					<div id="input-container">
+						<?php
+
+							if(empty($error))
+							{
+								echo "<label for=\"pwd\">Password</label>";
+								echo "<input type=\"password\" name=\"pwd\">";
+							}
+							else{
+								echo "<div id=\"error\"><label for=\"pwd\">Password</label> - ".$error[1]."</div>";
+								echo "<input id=\"inerror\" type=\"password\" name=\"pwd\">";
+							}
+
+						?>
+						<a id="forgot-pwd">Forgot your password?</a>
+					</div>
+					
+				</div>
+
 
 				<button type="submit" name="login-btn">Login</button>
+
+				<div>Don't have an account yet? <a href="Register">Register!</a></div>
 			</form>
-			<div>Don't have an account yet? <a href="Register">Register!</a></div>
+			
 		</main>
 	</body>
-<?php
-	require "welcome_page.footer.php";
-?>
+	<footer>
+	</footer>
+</html>
--- MVC/Views/User/register.php
+++ MVC/Views/User/register.php
@@ -1,19 +1,44 @@
-<?php
-	require "welcome_page.header.php";
-?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<meta charset="UTF-8">
+		<meta name="description" content="Header for the main page after the user has logged in.">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+		<link rel="stylesheet" href="/CSS/UserAccessForm.css">
+
+		<title>TwitchCord</title>
+	</head>
 	<body>
 		<main>
 			<form action="Register" method="post">
-				<input type="text" name="uid" placeholder="Username">
-				<input type="text" name="email" placeholder="Email">
-				<input type="password" name="pwd" placeholder="Password">
-				<input type="password" name="pwd-repeat" placeholder="Repeat Password">
+				<h1>Create Account</h1>
+				<div id="userinput">
+					<div id="input-container">
+						<label for="uid">Username</label>
+						<input type="text" name="uid">
+					</div>
+					<div id="input-container">
+						<label for="email">E-mail</label>
+						<input type="text" name="email">
+					</div>
+					<div id="input-container">
+						<label for="pwd">Password</label>
+						<input type="password" name="pwd">
+					</div>
+					<div id="input-container">
+						<label for="pwd-repeat">Repeat Password</label>
+						<input type="password" name="pwd-repeat">
+					</div>
+				</div>
 
 				<button type="submit" name="register-btn">Register</button>
-				<div>Already have an account?<a href="Login">Login</a></div>
+				<div>Already have an account? <a href="Login">Login</a></div>
 			</form>
 		</main>
 	</body>
-<?php
-	require "welcome_page.footer.php";
-?>
+	<footer>
+	</footer>
+</html>
