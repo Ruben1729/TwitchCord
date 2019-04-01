c9495cf3b5e78d50a67c3e7a1c700dae3790401e
cstuser <cstuser>
2019-04-01 13:50:10 -0400

    quick database layout

---
+++ tc.sql
@@ -0,0 +1,273 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 01, 2019 at 07:41 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banned`
+--
+
+CREATE TABLE `banned` (
+  `user_id` int(5) NOT NULL,
+  `channel_id` int(5) NOT NULL,
+  `banned_on` date NOT NULL,
+  `ban_binary` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel`
+--
+
+CREATE TABLE `channel` (
+  `channel_id` int(5) NOT NULL,
+  `channel_name` varchar(20) NOT NULL,
+  `description` varchar(80) NOT NULL,
+  `created_on` date NOT NULL,
+  `picture_id` int(5) NOT NULL,
+  `owner_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `follower`
+--
+
+CREATE TABLE `follower` (
+  `user_id` int(5) NOT NULL,
+  `channel_id` int(6) NOT NULL,
+  `followed_on` date NOT NULL,
+  `notification` int(1) NOT NULL,
+  `role_id` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group_chat`
+--
+
+CREATE TABLE `group_chat` (
+  `group_chat_id` int(6) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `channel_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `picture`
+--
+
+CREATE TABLE `picture` (
+  `picture_id` int(1) NOT NULL,
+  `path` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `profile_id` int(11) NOT NULL,
+  `user_id` int(5) NOT NULL,
+  `display_name` varchar(60) NOT NULL,
+  `bio` varchar(80) NOT NULL,
+  `created_on` date NOT NULL,
+  `picture_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `relationmodel`
+--
+
+CREATE TABLE `relationmodel` (
+  `user_id` int(11) NOT NULL,
+  `user_id_1` int(11) NOT NULL,
+  `status_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `status_id` int(5) NOT NULL,
+  `status_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usermodel`
+--
+
+CREATE TABLE `usermodel` (
+  `user_id` int(5) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `password_hash` varchar(80) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `banned`
+--
+ALTER TABLE `banned`
+  ADD PRIMARY KEY (`user_id`,`channel_id`);
+
+--
+-- Indexes for table `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`channel_id`),
+  ADD UNIQUE KEY `picture_id` (`picture_id`),
+  ADD UNIQUE KEY `owner_id` (`owner_id`);
+
+--
+-- Indexes for table `group_chat`
+--
+ALTER TABLE `group_chat`
+  ADD PRIMARY KEY (`group_chat_id`),
+  ADD UNIQUE KEY `channel_id` (`channel_id`),
+  ADD UNIQUE KEY `role_id` (`role_id`);
+
+--
+-- Indexes for table `picture`
+--
+ALTER TABLE `picture`
+  ADD PRIMARY KEY (`picture_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`profile_id`),
+  ADD UNIQUE KEY `picture_id` (`picture_id`);
+
+--
+-- Indexes for table `relationmodel`
+--
+ALTER TABLE `relationmodel`
+  ADD PRIMARY KEY (`user_id`,`user_id_1`),
+  ADD UNIQUE KEY `status_id` (`status_id`),
+  ADD UNIQUE KEY `user_id_2` (`user_id`),
+  ADD KEY `user_id` (`user_id`,`user_id_1`),
+  ADD KEY `user_id_1` (`user_id_1`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `usermodel`
+--
+ALTER TABLE `usermodel`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `channel`
+--
+ALTER TABLE `channel`
+  MODIFY `channel_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `group_chat`
+--
+ALTER TABLE `group_chat`
+  MODIFY `group_chat_id` int(6) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `picture`
+--
+ALTER TABLE `picture`
+  MODIFY `picture_id` int(1) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `profile_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `status_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `usermodel`
+--
+ALTER TABLE `usermodel`
+  MODIFY `user_id` int(5) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `channel`
+--
+ALTER TABLE `channel`
+  ADD CONSTRAINT `channel_ibfk_1` FOREIGN KEY (`channel_id`) REFERENCES `group_chat` (`channel_id`);
+
+--
+-- Constraints for table `picture`
+--
+ALTER TABLE `picture`
+  ADD CONSTRAINT `picture_ibfk_1` FOREIGN KEY (`picture_id`) REFERENCES `channel` (`picture_id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`picture_id`) REFERENCES `picture` (`picture_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `relationmodel`
+--
+ALTER TABLE `relationmodel`
+  ADD CONSTRAINT `relationmodel_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usermodel` (`user_id`),
+  ADD CONSTRAINT `relationmodel_ibfk_2` FOREIGN KEY (`user_id_1`) REFERENCES `usermodel` (`user_id`),
+  ADD CONSTRAINT `relationmodel_ibfk_3` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`);
+
+--
+-- Constraints for table `usermodel`
+--
+ALTER TABLE `usermodel`
+  ADD CONSTRAINT `usermodel_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `banned` (`user_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
