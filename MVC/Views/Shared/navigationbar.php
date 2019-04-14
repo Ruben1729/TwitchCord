fe10ddd4806e18d4f318cb877af1d72905cc8761
Michael Cannucci <gameactin@gmail.com>
2019-04-14 00:41:28 -0400

    Dropdown for signed in profile

--- MVC/Views/Shared/navigationbar.php
+++ MVC/Views/Shared/navigationbar.php
@@ -6,27 +6,21 @@
     <ul class="navbar">
         <li class="nav-item left"><a href="/Main/Index">TwitchCord</a></li>
         <?php if($uid != null){?>
-            <li class="nav-item right"><a href="/User/Logout">Logout</a></li>
-<<<<<<< HEAD
-            <li class="nav-item right"><a href="/Profile">Profile</a></li>
-            <li class="nav-item right"><a href="/Community/Index">Community</a></li>
-=======
-            <li class="nav-item right"><a href="/Profile/Settings">Profile</a></li>
-            <li class="nav-item right"><a href="/Channel/Dashboard">Channel</a></li>
->>>>>>> cebe2ae784199c2324b0c58883030519e199fe45
+            <div class="dropdown right">
+                <div class="dropProfile">
+                    <img class="left" src="/Pictures/default.png">
+                    <h3 class="left">USER</h3>
+                </div>
+                <div class="dropdown-content">
+                    <a href="/User/Logout">Logout</a>
+                    <a href="/Profile/Settings">Profile</a>
+                    <a href="/Channel/Dashboard">Channel</a>
+                    <a href="/Community/Index">Community</a>
+                </div>
+            </div>
         <?php }else{?>
             <li class="nav-item right"><a href="/User/Register">Register</a></li>
         	<li class="nav-item right"><a href="/User/Login">Login</a></li>
         <?php } ?>
     </ul>
 </div>
-
-<!-- I think it looks ugly for the navbar to flash in -->
-<!-- <script>
-    window.addEventListener("load", function() {
-        var elem = document.querySelector("#navigation");
-
-        elem.style.transition = '0.5s';
-        elem.style.opacity = '1';
-    })
-</script> -->
