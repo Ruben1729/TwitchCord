f10dec1ee84f4103a96a5063b5d4db51dd5f003f
Michael Cannucci <gameactin@gmail.com>
2019-04-16 22:36:25 -0400

    -Main Stream Showcase +Channel Search

--- MVC/Controllers/Community.php
+++ MVC/Controllers/Community.php
@@ -6,13 +6,21 @@
             $this->view('Community/index');
         }
 
-        public function Channel($channelRequested = null){
-            //Get user information
+        public function Channel(){
+            //Get channel/group_chat/user information and transfer it to JSON
             if(isset($_SESSION[username])){
                 $currentUser = $this->model('UserModel')->getUserSafe($_SESSION[username]);
+                $currentUser = json_encode($currentUser);
             }
-            //Get the user's channels
-            $this->view('Community/channel', ['channels' => $channels, 'user' => $currentUser]);
+
+            $channels = $this->model('ChannelModel')->getAllChannels();
+            $first_channel_id = $channels[0]->channel_id;
+            $channels = json_encode($channels);
+
+            $group_chats = $this->model('GroupChat')->getGroupChats($first_channel_id);
+            $group_chats = json_encode($group_chats);
+
+            $this->view('Community/channel', ['channels' => $channels, 'user' => $currentUser, 'group_chats' => $group_chats]);
         }
 
         public function POST_Follow(){
@@ -23,10 +31,12 @@
             ->Submit();
         }
 
-        public function ChannelList($username){
+        public function ChannelList(){
+            $channel_name = $_GET['channel_name'];
+
             $channels = $this
-            ->model('ChannelModel')
-            ->getChannels($username);
+                ->model('ChannelModel')
+                ->getChannels($channel_name);
             
             header('Content-Type: application/json');
             echo json_encode($channels);
--- MVC/Views/Main/index.php
+++ MVC/Views/Main/index.php
@@ -28,39 +28,6 @@
 			<button type="submit">GO</button>
             </form>
     </div>
-
-    <?php if (!empty($top_streamers)) { ?>
-    <div>
-        <h2 class="stream-title">Top Streams</h3>
-		<div id="stream-carousel">
-			<button onclick="carouselDirection(this)" class="carousel-btn" id="carousel-left">
-				&#129048;
-			</button>
-			<div class="stream-window window-1">
-				<div>
-
-				</div>
-			</div>
-			<div class="stream-window window-2">
-				<div id="highlight-stream">
-					<iframe src="https://player.twitch.tv/?collection=abcDeF1ghIJ2kL" height="300" width="500" frameborder="0" scrolling="no" allowfullscreen="true">
-					</iframe>
-				</div>
-			</div>
-			<div class="stream-window window-3">
-				<div>
-				</div>
-			</div>
-			<button onclick="carouselDirection(this)" class="carousel-btn" id="carousel-right">
-				&#129050;
-			</button>
-		</div>
-	</div>
-	<?php } else { ?>
-		<div id="no-stream">
-			<div id="banner">No one is currently streaming, maybe you can be the first?</div>
-		</div>
-    <?php } ?>
     <script src="/Javascript/Main.js"></script>
 </body>
 
