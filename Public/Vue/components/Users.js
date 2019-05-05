8ac019c7215d5e37474152edf1f71b6b637cc4f7
Michael Cannucci <gameactin@gmail.com>
2019-05-05 18:41:58 -0400

    Fixed active users Beginning of context

--- Public/Vue/components/Users.js
+++ Public/Vue/components/Users.js
@@ -1,40 +1,55 @@
 export default Vue.component('users', {
+    components: {
+        VueContext
+    },
     props: {
         channel_name: String,
         channel_id: Number,
     },
     data: function () {
         return {
-            users: [],
+            users: {},
         }
     },
     watch: {
         channel_name: function () {
             setTimeout(() => {
                 this.getUsers();
-                this.$socket.emit('get_online', this.channel_name);
             }, 200)
         },
     },
     methods: {
-        getUsers: function () {
+        getUsers: async function () {
             if (this.channel_name) {
                 axios.get('/Community/GetUsersFromChannel/' + this.channel_id)
                     .then(response => {
-                        this.users = response.data;
+                        let data = response.data;
+                        let _users = {};
+                        for (let i = 0; i < data.length; i++) {
+                            _users[data[i].user_id] = data[i];
+                        }
+                        this.users = _users;
+                    })
+                    .then(() => {
+                        this.$socket.emit('get_online', this.channel_name);
                     });
             }
         },
-        openMenu(e) {
-            console.log('test');
-            e.preventDefault();
+        kick(target, data) {
+            console.log('kicking user');
+        },
+        ban(target, data) {
+            console.log('banning user');
+            console.log(data);
         },
     },
     sockets: {
         online_users: function (online) {
-            for (let element in online) {
-                if (online[element.user_id]) {
-                    element.isActive = true;
+            for (let id in online) {
+                console.log(id);
+                console.log(this.users[id]);
+                if (this.users[id]) {
+                    this.users[id].isActive = true;
                 }
             };
         },
@@ -43,17 +58,20 @@ export default Vue.component('users', {
         },
     },
     template: `
-    <div id="users-bar" :contextmenu="openMenu">
+    <div id="users-bar">
         <ul>
-            <li v-for="user in users">
-                <template v-if="user.isActive">
-                    Active: {{user.username}}
-                </template>
-                <template v-else>
-                    {{user.username}}
-                </template>
+            <li v-for="user in users" @contextmenu.prevent="$refs.menu.open($event, user)" 
+                :class="['user', user.isActive ? 'active' : '']">
+                {{user.username}}
             </li>
         </ul>
+
+        <vue-context ref="menu">
+            <ul slot-scope="child">
+                <li @click="kick($event.target, child.data)">Kick</li>
+                <li @click="ban($event.target, child.data)">Ban</li>
+            </ul>
+        </vue-context>
     </div>
     `,
 });
